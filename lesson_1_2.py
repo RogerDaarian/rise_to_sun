e34bfc9ff7527e5c742c1ed986423600432d97d3
RogerDaarian <75474385+RogerDaarian@users.noreply.github.com>
2020-12-05 03:06:53 +0300

    Update lesson_1_2.py

--- lesson_1_2.py
+++ lesson_1_2.py
@@ -1,7 +1,8 @@
 print("Second")
-
+# 
 some_data = int(input("Введите число в секундах: "))
 h = some_data // 3600
 m = (some_data // 60) % 60
 s = some_data % 60
 print("Time info: %02d:%02d:%02d" % (h, m, s))
+# 
