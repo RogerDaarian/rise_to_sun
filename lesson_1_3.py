c81b7ab778ea141bdc7f232991ed87d0059f9a6d
RogerDaarian <75474385+RogerDaarian@users.noreply.github.com>
2020-12-05 03:12:06 +0300

    Delete lesson_1_3.py

--- lesson_1_3.py
+++
@@ -1,8 +0,0 @@
-print("Summ")
-
-number = int(input("Введите число от 1 до 9"))
-second = number * 11
-third = number * 111
-conglutination = f"Summ: {number} + {second} + {third}"
-result = int(number) + int(second) + int(third)
-print("Result: %s = %d" % (conglutination, result))
